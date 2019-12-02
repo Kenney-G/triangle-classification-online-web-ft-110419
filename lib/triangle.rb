b9bb3ea891cf576005379934272c002a75c745a2
Ken Goad <goadke@g.cofc.edu>
2019-12-02 17:19:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,6 +23,7 @@ class Triangle
         if side_one + side_two > side_three || side_two + side_three > side_one || side_one + side_three > side_two
         true
         else
+          false
             raise TriangleError
             puts "The angles for this triangle are invalid."
         end
