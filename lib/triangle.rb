1065723cf902c29110e78f5e52cb7b7e9fca9587
Ken Goad <goadke@g.cofc.edu>
2019-12-02 16:42:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,10 +13,8 @@ class Triangle
         elsif
           side_one == side_two || side_one == side_three || side_two == side_three
       :isosceles
-          
-        
         else
-          scalene
+      :scalene
       end
     end
     
