1294ec8b414cf8b79c55a4aa729af95b9f567583
Ken Goad <goadke@g.cofc.edu>
2019-12-02 16:46:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,14 +20,18 @@ class Triangle
     
     def valid?
       if side_one + side_two > side_three || side_two + side_three > side_one || side_one + side_three > side_two
+      true
+        elsif
+          side_one > 0 && side_two > 0 && side_three > 0
+        true
+      else
+        false
       end
-        if side_one > 0 && side_two > 0 && side_three > 0
-        end
       if valid? == false
         begin
           raise TriangleError
           puts "The angles for this triangle are invalid."
-        end
+       end
       end
     end
     end
